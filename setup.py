fbf00435c9ab8ef0f393caf66d94d01ca268d594
DerThorsten <derthorstenbeier@gmail.com>
2023-04-19 09:57:52 +0200

    using empack 3.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-requirements = ["typer", "playwright", "appdirs", "empack >=2.0.0", "rich"]
+requirements = ["typer", "playwright", "appdirs", "empack >=3.0.0", "rich"]
 
 setup(
     author="Thorsten Beier",
